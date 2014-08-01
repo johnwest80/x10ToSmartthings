e814b0fbbe53fa3b3e2c574808f6ff72d91bc055
john west <jwest@premierlogic.com>
2014-08-01 08:14:20 -0400

    Still with my code trying to figure out why it wasn't working.  Turns out it was the rs232 port that went bad.

--- x10ToSmartthings/x10.cs
+++ x10ToSmartthings/x10.cs
@@ -1,6 +1,9 @@
 using System;
 using System.Collections;
 using System.IO.Ports;
+using System.Threading;
+using Microsoft.SPOT;
+using Math = System.Math;
 
 namespace x10tosmartthings
 {
@@ -24,6 +27,15 @@ namespace x10tosmartthings
         public IList Queue { get; set; }
         public static event ActionDelegate ActionEvent;
 
+        public enum cm11aInitStatus
+        {
+            NoneInited,
+            MacrosInited,
+            AllInited
+        }
+
+        private cm11aInitStatus curCm11aInitStatus = cm11aInitStatus.AllInited;
+
         public void QueueDeviceCommand(Command command)
         {
             Queue.Add(command);
@@ -36,11 +48,35 @@ namespace x10tosmartthings
             if (Serial.BytesToRead > 0)
             {
                 int b = Serial.ReadByte();
-                if (b == 90)
+                if (b == 0xa5) //startup poll for the cm11a
+                {
+                    ProcessInitRequest();
+                }
+                else if (b == 90)
                     ProcessIncoming();
+                else
+                    Logging.DebugPrint("Rcvd: " + ((char)b).ToString());
+
             }
-            if (Queue.Count > 0)
-                TalkToDevice((Command) Queue[0]);
+            //else if (Queue.Count > 0)
+                //TalkToDevice((Command)Queue[0]);
+        }
+
+        private void ProcessInitRequest()
+        {
+            switch (curCm11aInitStatus)
+            {
+                case cm11aInitStatus.AllInited:
+                {
+                    ProcessCm11AMacroInit();
+                    break;
+                }
+                case cm11aInitStatus.MacrosInited:
+                {
+                    ProcessCm11ATimeInit();
+                    break;
+                }
+            }
         }
 
         private void TalkToDevice(Command command)
@@ -52,7 +88,7 @@ namespace x10tosmartthings
                 //send address
                 var bytes = new byte[2];
                 bytes[0] = 0x04;
-                bytes[1] = (byte) (X10Helper.GetHouseCode(command.HouseCode) | (int) (X10Helper.GetDeviceCode(int.Parse(command.DeviceCode))));
+                bytes[1] = (byte)(X10Helper.GetHouseCode(command.HouseCode) | (int)(X10Helper.GetDeviceCode(int.Parse(command.DeviceCode))));
                 //Logging.DebugPrint("Talk 2 bytes: " + bytes[0] + "/" + bytes[1]);
                 Serial.Write(bytes, 0, bytes.Length);
                 //verify checksum
@@ -72,7 +108,7 @@ namespace x10tosmartthings
                 {
                     var actionbytes = new byte[2];
                     actionbytes[0] = 0x06;
-                    actionbytes[1] = (byte) (X10Helper.GetHouseCode(command.HouseCode) | (int) (X10Helper.GetFunctionFromName(command.Action)));
+                    actionbytes[1] = (byte)(X10Helper.GetHouseCode(command.HouseCode) | (int)(X10Helper.GetFunctionFromName(command.Action)));
                     //Logging.DebugPrint("Talk action 2 bytes: " + actionbytes[0] + "/" + actionbytes[1]);
                     Serial.Write(actionbytes, 0, actionbytes.Length);
 
@@ -94,6 +130,10 @@ namespace x10tosmartthings
                             Logging.DebugPrint("Diverted from getting conf to processing incoming");
                             ProcessIncoming();
                         }
+                        else if (interfaceReady == 0xa5)
+                        {
+                            ProcessInitRequest();
+                        }
                         else
                             throw new Exception("Talk action conf error - received byte " + interfaceReady);
                     }
@@ -108,6 +148,10 @@ namespace x10tosmartthings
                     {
                         Logging.DebugPrint("Diverted from getting talk conf to processing incoming");
                         ProcessIncoming();
+                    }
+                    else if (talkConf == 0xa5)
+                    {
+                        ProcessInitRequest();
                     }
                     else
                         throw new Exception("Talk conf error - received byte " + talkConf);
@@ -125,6 +169,33 @@ namespace x10tosmartthings
             }
         }
 
+        private void ProcessCm11AMacroInit()
+        {
+            ProcessCm11ATimeInit();
+            return;
+            Logging.DebugPrint("== cm11a macro init");
+            Serial.WriteByte(0xfb); //respond
+            var bytes = new byte[42];//we don't have macros, so just send all zeros
+            Serial.Write(bytes, 0, bytes.Length);//has to be 42 bytes
+            Serial.ReadByte();//get checksum - ignore
+            Serial.WriteByte(0);//checksum correct
+            curCm11aInitStatus = cm11aInitStatus.MacrosInited;
+        }
+
+
+        private void ProcessCm11ATimeInit()
+        {
+            Logging.DebugPrint("== cm11a time init");
+            //Serial.Flush();
+            //var bytes = new byte[] { 0x9b, 0x13, 0x40, 0x0a, 0x48, 0x82, 0x60 };
+            //var bytes = new byte[] {0x9b, 0, 0, 0, 0, 1, 0x61};
+            var bytes = new byte[] { 0x9b };
+            Serial.Write(bytes, 0, bytes.Length);//has to be 42 bytes
+            //var b = Serial.ReadByte();//get checksum - ignore
+            //Serial.WriteByte(0);//checksum correct
+            curCm11aInitStatus = cm11aInitStatus.AllInited;
+        }
+
         private static void ProcessIncoming()
         {
             Serial.Flush();
@@ -137,16 +208,22 @@ namespace x10tosmartthings
 
             //first byte is function/address mask
             var functionBitFlag = Serial.ReadByte();
+
+            if (numBytes > 8) // can't have more than 8 bytes
+            {
+                Logging.DebugPrint("Err buf size: " + numBytes);
+                return;
+            }
             //Logging.DebugPrint("Raw function/address val is " + functionBitFlag);
             numBytes--;
 
             var curByte = 0; //current data byte after num bytes and mask being looked at in the buffer that was sent
 
             //gotta have some data to be able to do anything
-            while (numBytes > 0 && numBytes <= 8) // can't have more than 8 bytes
+            while (numBytes > 0) 
             {
                 //determine whether it's a function or address coming next
-                var function = (functionBitFlag & (int) Math.Pow(2, curByte)) == (int) Math.Pow(2, curByte);
+                var function = (functionBitFlag & (int)Math.Pow(2, curByte)) == (int)Math.Pow(2, curByte);
                 var address = !function;
                 //Logging.DebugPrint("Curbyte " + curByte + " function/address: " + function + "/" + address);
 
@@ -158,16 +235,16 @@ namespace x10tosmartthings
 
                 if (address)
                 {
-                    var housecode = (X10Helper.HouseCode) (firstByte & (128 + 64 + 32 + 16));
-                    int devicecode = X10Helper.GetDeviceNumber((X10Helper.DeviceCode) (firstByte & (8 + 4 + 2 + 1)));
+                    var housecode = (X10Helper.HouseCode)(firstByte & (128 + 64 + 32 + 16));
+                    int devicecode = X10Helper.GetDeviceNumber((X10Helper.DeviceCode)(firstByte & (8 + 4 + 2 + 1)));
                     //we're not handling situations where multiple devices are acted on at the same time
                     LastIncomingAddress = X10Helper.GetDeviceLetter(housecode) + devicecode;
                     //Logging.DebugPrint("LastIncomingAddress set to " + LastIncomingAddress);
                 }
                 if (function && LastIncomingAddress != null)
                 {
-                    var housecode = (X10Helper.HouseCode) (firstByte & (128 + 64 + 32 + 16));
-                    string action = X10Helper.GetFunctionName((X10Helper.Function) (firstByte & (8 + 4 + 2 + 1)));
+                    var housecode = (X10Helper.HouseCode)(firstByte & (128 + 64 + 32 + 16));
+                    string action = X10Helper.GetFunctionName((X10Helper.Function)(firstByte & (8 + 4 + 2 + 1)));
 
                     if (ActionEvent != null)
                     {
